37b73bfafb2a41a63d157d42a4b28773847d4cfe
Zachary Godfrey <zachary.godfrey@bamtech.net>
2021-07-30 09:15:27 -0400

    Use type instead of interface

--- src/types.ts
+++ src/types.ts
@@ -1,36 +1,36 @@
 import { QueryResult } from 'pg';
 
-export interface AppEvent {
+export type AppEvent = {
   type: string;
   time: string;
   data: any;
-}
+};
 
-export interface AppRequest {
+export type AppRequest = {
   id: string;
   time: string;
   token: string;
   action: string;
   data: any;
   user?: any;
-}
+};
 
-export interface AppResponse {
+export type AppResponse = {
   status: 200 | 400 | 404 | 500;
   body: {
     data: any;
     messages: string[];
   };
-}
+};
 
-export interface Action {
+export type Action = {
   name: string;
   authenticate: boolean;
   validate: (request: AppRequest, db: Database) => Promise<string[]>;
   execute: (request: AppRequest, db: Database) => Promise<AppResponse>;
-}
+};
 
-export interface Database {
+export type Database = {
   query: (sql: string, values?: any[] | undefined) => Promise<QueryResult<any>>;
   insert: (tableName: string, params: Record<string, any>) => Promise<any[]>;
-}
+};
